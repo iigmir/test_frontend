e4aa0f98f5ec25ccce471b02ef09290b55dcb847
iigmir <roc120j@gmail.com>
2025-01-04 00:09:30 +0800

    Develop the <BFormControls /> component

--- src/components/BFormControls.vue
+++ src/components/BFormControls.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <label for="" class="form-label"></label>
-    <input type="" class="form-control" id="" placeholder="">
+    <label :for="id_bind" class="form-label">{{ props.label }}</label>
+    <input :type="type_to_bind" class="form-control" :id="id_bind" placeholder="" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 
+const props = defineProps({
+  label: String,
+  type: String
+});
+
+const type_to_bind = computed( () => props.type ?? "text" );
+const id_bind = computed( () => `label-${props.label}-${type_to_bind.value}` );
 </script>
 
 <style lang="scss" scoped>
